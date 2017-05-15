0eea731009965e033c9a3d2c42bca3292d348adc
Dang Nguyen <haidangibm@gmail.com>
2017-05-15 10:45:49 +0700

    Update Helper method

--- src/Helpers/KeylistHelper.php
+++ src/Helpers/KeylistHelper.php
@@ -4,8 +4,12 @@ namespace Delatbabel\Keylists\Helpers;
 
 class KeylistHelper {
     public static function filterByKeytypeName($query, $name) {
-        $query->where(
-            'keytype_id', \Delatbabel\Keylists\Models\Keytype::where('name', $name)->first()->id
-        );
+        $obj = \Delatbabel\Keylists\Models\Keytype::where('name', $name)->first();
+        if ($obj) {
+            $query->where('keytype_id', $obj->id);
+        } else {
+            // Force the query return null
+            $query->where('keytype_id', 0);
+        }
     }
 }
